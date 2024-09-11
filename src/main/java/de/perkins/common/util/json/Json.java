ee26b5646e3b99f380e0203a94b098812e5fef28
BPerkins97 <benjamin-erik-perkins@outlook.com>
2024-09-11 19:34:08 +0200

    Two fields

--- src/main/java/de/perkins/common/util/json/Json.java
+++ src/main/java/de/perkins/common/util/json/Json.java
@@ -8,44 +8,54 @@ public class Json {
     private static final int MODE_VALUE = 4;
 
     public static JsonNode parse(String json) {
-        if (json.codePointAt(0) != '{') {
+        int numCodePoints = json.codePointCount(0, json.length());
+        int pointer = 0;
+        while (pointer < numCodePoints && isWhitespace(json.codePointAt(pointer))) {
+            pointer++;
+        }
+        if (json.codePointAt(pointer) != '{') {
             throw new IllegalArgumentException("A valid JSON has to start with a curly brace '{'.");
         }
 
         int previousMode = MODE_OBJECT_START;
         int mode = MODE_WHITESPACE_CONSUME;
-        int numCodePoints = json.codePointCount(0, json.length());
         JsonNode node = new JsonNode();
         StringBuilder builder = new StringBuilder();
         String value = null;
 
-        for (int i=1;i<numCodePoints;i++) {
+        for (int i=pointer+1;i<numCodePoints;i++) {
             int codePoint = json.codePointAt(i);
             switch (mode) {
                 case MODE_WHITESPACE_CONSUME -> {
                     if (!isWhitespace(codePoint)) {
                         switch (previousMode) {
                             case MODE_OBJECT_START -> {
-                                // TODO throw exception if this char is not a quote
+                                if (codePoint != '"') {
+                                    throw new IllegalArgumentException("A key has to be quoted.");
+                                }
                                 previousMode = MODE_WHITESPACE_CONSUME;
                                 mode = MODE_KEY;
                             }
                             case MODE_KEY -> {
-                                // TODO throw exception if this char is not a :
+                                if (codePoint != ':') {
+                                    throw new IllegalArgumentException("A key definition has to be followed by a colon.");
+                                }
                                 previousMode = MODE_ASSIGNMENT;
                             }
                             case MODE_ASSIGNMENT -> {
                                 mode = MODE_VALUE;
                                 builder.appendCodePoint(codePoint);
                             }
-                        }
-                        if (previousMode == MODE_OBJECT_START) {
-
+                            case MODE_VALUE -> {
+                                // TODO throw exception when is not quote
+                                previousMode = MODE_WHITESPACE_CONSUME;
+                                mode = MODE_KEY;
+                            }
                         }
                     }
                 }
                 case MODE_KEY -> {
-                    if (codePoint == '"' && json.codePointAt(i-1) != '\\') {
+                    if (codePoint == '"') {
                         previousMode = MODE_KEY;
                         mode = MODE_WHITESPACE_CONSUME;
                         value = builder.toString();
@@ -59,7 +69,12 @@ public class Json {
                         previousMode = MODE_VALUE;
                         mode = MODE_WHITESPACE_CONSUME;
                         node.putField(value, Integer.parseInt(builder.toString()));
-                    } else {
+                    } else if (codePoint == ',') {
+                        mode = MODE_WHITESPACE_CONSUME;
+                        previousMode = MODE_VALUE;
+                        node.putField(value, Integer.parseInt(builder.toString()));
+                        builder = new StringBuilder();
+                    }else {
                         builder.appendCodePoint(codePoint);
                     }
                 }
--- src/test/java/de/perkins/common/util/json/JsonTest.java
+++ src/test/java/de/perkins/common/util/json/JsonTest.java
@@ -54,6 +54,60 @@ public class JsonTest {
                         }
                         """;
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> Json.parse(json));
+        IllegalArgumentException illegalArgumentException = Assertions.assertThrows(IllegalArgumentException.class, () -> Json.parse(json));
+        Assertions.assertEquals("A valid JSON has to start with a curly brace '{'.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenKeyIsNotFollowedByColon() {
+        String json = """
+                       {
+                            "key" 2
+                        }
+                        """;
+
+        IllegalArgumentException illegalArgumentException = Assertions.assertThrows(IllegalArgumentException.class, () -> Json.parse(json));
+        Assertions.assertEquals("A key definition has to be followed by a colon.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenNotQuotingAKey() {
+        String json = """
+                       {
+                            key" 2
+                        }
+                        """;
+
+        IllegalArgumentException illegalArgumentException = Assertions.assertThrows(IllegalArgumentException.class, () -> Json.parse(json));
+        Assertions.assertEquals("A key has to be quoted.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    public void shouldParseTwoFields() {
+        String json = """
+                       {
+                            "key1": 2,
+                            "key2": 3
+                        }
+                        """;
+
+        JsonNode node = Json.parse(json);
+        Assertions.assertTrue(node.isObject());
+        Assertions.assertEquals(2, node.getInt("key1"));
+        Assertions.assertEquals(3, node.getInt("key2"));
+    }
+
+    @Test
+    public void shouldIgnoreLeadingWhitespace() {
+        String json = """
+                       \n\t\r {
+                            "key1": 2
+                        }
+                        """;
+
+        JsonNode node = Json.parse(json);
+        Assertions.assertTrue(node.isObject());
+        Assertions.assertTrue(node.hasField("key1"));
+        Assertions.assertEquals(2, node.getInt("key1"));
     }
 }
